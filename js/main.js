f7e8d6cbb5793ea4cdb5d97b0876b92a11f80c82
Kitty Hurley <hockeyduck30@gmail.com>
2015-08-22 21:56:20 -0500

    Upgraded to 3.14
    Upgraded to Esri JS 3.14, added a home button, changed the basemap (from
    OSM to Topo), Changed the extent to the state of Minnesota.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 var map;
-	require(["esri/map", "dojo/domReady!"], function(Map) {
-		map = new Map("map", {
-			basemap: "osm",
-			center: [-122.45, 37.75], // longitude, latitude
-			zoom: 13
-        });
+	require([
+	         "esri/map", 
+	         "esri/dijit/HomeButton",
+	         "dojo/domReady!"
+   ], 	
+   
+function (Map, HomeButton) {
+	
+	var map = new Map("map", {
+			basemap: "topo",
+			center: [-93.5, 46.5],
+			zoom: 6
 	});
+	
+	var home = new HomeButton ({ //Home Button
+	    map: map
+	}, "HomeButton");
+	
+	//Startup
+	home.startup();
+
+});
